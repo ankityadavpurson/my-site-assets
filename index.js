a287653706926d54069bf45472d88d2a2110b378
Ankit Yadav <ankityadavpurson@gmail.com>
2022-01-25 00:56:58 +0530

    cors fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const hostName = require("./src/host");
 const { assets, counter } = require("./src/router");
@@ -6,6 +7,7 @@ const { home } = require("./src/templates");
 const dbConnect = require("./src/db");
 
 const app = express();
+app.use(cors());
 
 app.get("/", home);
 app.use("/assets", assets);
